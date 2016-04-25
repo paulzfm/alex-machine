4942c0cdd396ebdb53aa5a7e5d39178d67bf6229
Paul Zhu <zhufengminpaul@163.com>
2016-04-25 21:11:53 +0800

    seperate int32 operations

--- simulator/alex-cpu.js
+++ simulator/alex-cpu.js
@@ -1,29 +1,16 @@
+var bin = require('./binary');
+
 var
   PC = 0,
-  regs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  regs = [],
   FP = 11,
   SP = 12,
   GP = 13,
   AT = 14,
   LR = 15,
 
-  fregs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  fregs = [],
   mem = {},
-
-  signed_ext = function (imm) {
-    if (imm >> 15 == 1) {
-      return (0xFFFF0000 + imm) >> 0;
-    }
-    return imm;
-  },
-
-  unsigned_ext = function (imm) {
-    return imm;
-  },
-
-  offset_ext = function (imm) {
-    return imm >> 2;
-  },
 
   decodeRType = function (ins) {
     return {
@@ -57,51 +44,6 @@ var
     };
   },
 
-  exeLogicR = function (op) {
-    return function (args) {
-      regs[args['ra']] = op(regs[args['rb']], regs[args['rc']]) ? 1 : 0;
-    };
-  },
-
-  opAdd = function (a, b) {
-    return ((a + b) & 0xFFFFFFFF) >> 0;
-  },
-
-  opSub = function (a, b) {
-    return ((a - b) & 0xFFFFFFFF) >> 0;
-  },
-
-  opMul = function (a, b) {
-    return ((a * b) & 0xFFFFFFFF) >> 0;
-  },
-
-  opDiv = function (a, b) {
-    return;
-  },
-
-  opMod = function (a, b) {
-    return ((a % b) & 0xFFFFFFFF) >> 0;
-  },
-
-  opShl = function (a, b) {
-    // assert(0 <= b && b <= 32);
-    return a << b;
-  },
-
-  opSar = function (a, b) {
-    assert(0 <= b && b <= 32);
-    return a >> b;
-  },
-
-  opSlr = function (a, b) {
-    assert(0 <= b && b <= 32);
-    return a >>> b;
-  },
-
-  opOr = function (a, b) {
-    return a | b;
-  },
-
   exeBranch = function (test) {
     return function (args) {
       if (test(regs[args['ra']], regs[args['rb']])) {
@@ -112,41 +54,15 @@ var
     };
   },
 
-  load = function (bytes) {
-    return function (addr) {
-      var data = 0;
-      for (var i = 0; i < bytes; i++) {
-        data += mem[addr + i] << (8 * i);
-      }
-    };
-  },
-
-  loadWord = load(4),
-  loadHalf = load(2),
-  loadByte = load(1),
-
-  store = function (bytes) {
-    return function (addr, data) {
-      for (var i = 0; i < bytes; i++) {
-        mem[addr + i] = data & 0xFF;
-        data >>> 8;
-      }
-    };
-  },
-
-  storeWord = store(4),
-  storeHalf = store(2),
-  storeByte = store(1),
-
   exeLoad = function (loader) {
     return function (args) {
-      regs[args['ra']] = loader(regs[args['rb']] + args['imm']);
+      regs[args['ra']] = loader(regs[args['rb']] + args['imm'], mem);
     };
   },
 
   exeStore = function (saver) {
     return function (args) {
-      saver(regs[args['rb']] + args['imm'], regs[args['ra']]);
+      saver(regs[args['rb']] + args['imm'], regs[args['ra']], mem);
     };
   },
 
@@ -164,107 +80,67 @@ var
     };
   },
 
-  unsignedLT = function (a, b) {
-    if ((a >= 0 && b >= 0) || (a < 0 && b < 0)) {
-      return a < b;
-    } else {
-      return a >= 0;
-    }
-  },
-
   insTable = {
     0x00: executor(decodeRType, function () {
     }, cont),
 
-    0x01: executor(decodeRType, exeBinR(opAdd), cont),
-    0x02: executor(decodeIType(signed_ext), exeBinI(opAdd), cont),
-    0x03: executor(decodeIType(unsigned_ext), exeBinI(opAdd), cont),
+    0x01: executor(decodeRType, exeBinR(bin.add32), cont),
+    0x02: executor(decodeIType(bin.ext32), exeBinI(bin.add32), cont),
+    0x03: executor(decodeIType(bin.uext32), exeBinI(bin.add32), cont),
 
-    0x04: executor(decodeRType, exeBinR(opSub), cont),
-    0x05: executor(decodeIType(signed_ext), exeBinI(opSub), cont),
-    0x06: executor(decodeIType(unsigned_ext), exeBinI(opSub), cont),
+    0x04: executor(decodeRType, exeBinR(bin.sub32), cont),
+    0x05: executor(decodeIType(bin.ext32), exeBinI(bin.sub32), cont),
+    0x06: executor(decodeIType(bin.uext32), exeBinI(bin.sub32), cont),
 
-    0x07: executor(decodeRType, exeBinR(opMul), cont),
-    0x08: executor(decodeIType(signed_ext), exeBinI(opMul), cont),
-    0x09: executor(decodeIType(unsigned_ext), exeBinI(opMul), cont),
+    0x07: executor(decodeRType, exeBinR(bin.mul32), cont),
+    0x08: executor(decodeIType(bin.ext32), exeBinI(bin.mul32), cont),
+    0x09: executor(decodeIType(bin.uext32), exeBinI(bin.mul32), cont),
 
-    0x0A: executor(decodeRType, exeBinR(opDiv), cont),
-    0x0B: executor(decodeIType(signed_ext), exeBinI(opDiv), cont),
-    0x0C: executor(decodeIType(unsigned_ext), exeBinI(opDiv), cont),
+    0x0A: executor(decodeRType, exeBinR(bin.div32), cont),
+    0x0B: executor(decodeIType(bin.ext32), exeBinI(bin.div32), cont),
+    0x0C: executor(decodeIType(bin.uext32), exeBinI(bin.div32), cont),
 
-    0x0D: executor(decodeRType, exeBinR(opMod), cont),
-    0x0E: executor(decodeIType(signed_ext), exeBinI(opMod), cont),
-    0x0F: executor(decodeIType(unsigned_ext), exeBinI(opMod), cont),
+    0x0D: executor(decodeRType, exeBinR(bin.mod32), cont),
+    0x0E: executor(decodeIType(bin.ext32), exeBinI(bin.mod32), cont),
+    0x0F: executor(decodeIType(bin.uext32), exeBinI(bin.mod32), cont),
 
-    0x10: executor(decodeRType, exeBinR(opShl), cont),
-    0x11: executor(decodeIType(unsigned_ext), exeBinI(opShl), cont),
+    0x10: executor(decodeRType, exeBinR(bin.shl32), cont),
+    0x11: executor(decodeIType(bin.uext32), exeBinI(bin.shl32), cont),
 
-    0x12: executor(decodeRType, exeBinR(opSlr), cont),
-    0x13: executor(decodeIType(unsigned_ext), exeBinI(opSlr), cont),
+    0x12: executor(decodeRType, exeBinR(bin.slr32), cont),
+    0x13: executor(decodeIType(bin.uext32), exeBinI(bin.slr32), cont),
 
-    0x14: executor(decodeRType, exeBinR(opSar), cont),
-    0x15: executor(decodeIType(unsigned_ext), exeBinI(opSar), cont),
+    0x14: executor(decodeRType, exeBinR(bin.sar32), cont),
+    0x15: executor(decodeIType(bin.uext32), exeBinI(bin.sar32), cont),
 
-    0x16: executor(decodeRType, exeBinR(function (a, b) {
-      return a & b;
-    }), cont),
-    0x17: executor(decodeRType, exeBinR(opOr), cont),
-    0x42: executor(decodeIType(unsigned_ext), exeBinI(opOr), cont),
-    0x18: executor(decodeRType, exeBinR(function (a, b) {
-      return a ^ b;
-    }), cont),
-    0x19: executor(decodeRType, exeBinR(function (a, b) {
-      return ~a;
-    }), cont),
-    0x1A: executor(decodeRType, exeLogicR(function (a, b) {
-      return a == b;
-    }), cont),
-    0x1B: executor(decodeRType, exeLogicR(function (a, b) {
-      return a != b;
-    }), cont),
-    0x1C: executor(decodeRType, exeLogicR(function (a, b) {
-      return a < b;
-    }), cont),
-    0x1D: executor(decodeRType, exeLogicR(unsignedLT), cont),
-    0x1E: executor(decodeRType, exeLogicR(function (a, b) {
-      return a > b;
-    }), cont),
-    0x1F: executor(decodeRType, exeLogicR(function (a, b) {
-      return a != b && !unsignedLT(a, b);
-    }), cont),
-    0x20: executor(decodeRType, exeLogicR(function (a, b) {
-      return a <= b;
-    }), cont),
-    0x21: executor(decodeRType, exeLogicR(function (a, b) {
-      return a == b || unsignedLT(a, b);
-    }), cont),
-    0x22: executor(decodeRType, exeLogicR(function (a, b) {
-      return a >= b;
-    }), cont),
-    0x23: executor(decodeRType, exeLogicR(function (a, b) {
-      return !unsignedLT(a, b);
-    }), cont),
+    0x16: executor(decodeRType, exeBinR(bin.and32), cont),
+    0x17: executor(decodeRType, exeBinR(bin.or32), cont),
+    0x42: executor(decodeIType(bin.uext32), exeBinI(bin.or32), cont),
+    0x18: executor(decodeRType, exeBinR(bin.xor32), cont),
+    0x19: executor(decodeRType, exeBinR(bin.not32), cont),
+    0x1A: executor(decodeRType, exeBinR(bin.eq32), cont),
+    0x1B: executor(decodeRType, exeBinR(bin.ne32), cont),
+    0x1C: executor(decodeRType, exeBinR(bin.lt32), cont),
+    0x1D: executor(decodeRType, exeBinR(bin.ltu32), cont),
+    0x1E: executor(decodeRType, exeBinR(bin.gt32), cont),
+    0x1F: executor(decodeRType, exeBinR(bin.gtu32), cont),
+    0x20: executor(decodeRType, exeBinR(bin.le32), cont),
+    0x21: executor(decodeRType, exeBinR(bin.leu32), cont),
+    0x22: executor(decodeRType, exeBinR(bin.ge32), cont),
+    0x23: executor(decodeRType, exeBinR(bin.geu32), cont),
 
-    0x24: executor(decodeIType(offset_ext), exeBranch(function (a, b) {
+    0x24: executor(decodeIType(bin.oext32), exeBranch(function () {
       return true;
     }), jmp),
-    0x25: executor(decodeIType(offset_ext), exeBranch(function (a, b) {
-      return a == b;
-    }), jmp),
-    0x26: executor(decodeIType(offset_ext), exeBranch(function (a, b) {
-      return a != b;
-    }), jmp),
-    0x27: executor(decodeIType(offset_ext), exeBranch(function (a, b) {
-      return a < b;
-    }), jmp),
-    0x28: executor(decodeIType(offset_ext), exeBranch(function (a, b) {
-      return a > b;
-    }), jmp),
+    0x25: executor(decodeIType(bin.oext32), exeBranch(bin.eq32), jmp),
+    0x26: executor(decodeIType(bin.oext32), exeBranch(bin.ne32), jmp),
+    0x27: executor(decodeIType(bin.oext32), exeBranch(bin.lt32), jmp),
+    0x28: executor(decodeIType(bin.oext32), exeBranch(bin.gt32), jmp),
 
     0x29: executor(function (ins) {
       return {
         'code': ins >> 24,
-        'imm': unsigned_ext(ins << 8)
+        'imm': bin.uext32(ins << 8)
       };
     }, function (args) {
       return (PC & 0xFC000000) + args['imm'] << 2;
@@ -273,34 +149,34 @@ var
       return regs[args['ra']];
     }, jmp),
     0x2B: executor(decodeRType, function (args) {
-      var data = loadWord(PC + 4);
-      regs[SP] -= 4;
-      storeWord(regs[SP], data);
-      return args['ra'];
+      //var data = loadWord(PC + 4);
+      //regs[SP] -= 4;
+      //storeWord(regs[SP], data);
+      //return args['ra'];
     }, jmp),
     0x2C: executor(decodeRType, function (args) {
-      var data = loadWord(regs[SP]);
-      regs[SP] += 4;
-      return data;
+      //var data = loadWord(regs[SP]);
+      //regs[SP] += 4;
+      //return data;
     }, jmp),
 
-    0x2D: executor(decodeIType(signed_ext), exeLoad(loadWord), cont),
-    0x2E: executor(decodeIType(signed_ext), exeLoad(loadHalf), cont),
-    0x2F: executor(decodeIType(signed_ext), exeLoad(loadByte), cont),
+    0x2D: executor(decodeIType(bin.ext32), exeLoad(bin.loadWord), cont),
+    0x2E: executor(decodeIType(bin.ext32), exeLoad(bin.loadHalf), cont),
+    0x2F: executor(decodeIType(bin.ext32), exeLoad(bin.loadByte), cont),
 
-    0x31: executor(decodeIType(signed_ext), function (args) {
+    0x31: executor(decodeIType(bin.ext32), function (args) {
       regs[args['ra']] = args['imm'];
     }, cont),
-    0x32: executor(decodeIType(unsigned_ext), function (args) {
+    0x32: executor(decodeIType(bin.uext32), function (args) {
       regs[args['ra']] = args['imm'];
     }, cont),
-    0x33: executor(decodeIType(unsigned_ext), function (args) {
+    0x33: executor(decodeIType(bin.uext32), function (args) {
       regs[args['ra']] = args['imm'] << 16;
     }, cont),
 
-    0x34: executor(decodeIType(signed_ext), exeStore(storeWord), cont),
-    0x35: executor(decodeIType(signed_ext), exeStore(storeHalf), cont),
-    0x36: executor(decodeIType(signed_ext), exeStore(storeByte), cont),
+    0x34: executor(decodeIType(bin.ext32), exeStore(bin.storeWord), cont),
+    0x35: executor(decodeIType(bin.ext32), exeStore(bin.storeHalf), cont),
+    0x36: executor(decodeIType(bin.ext32), exeStore(bin.storeByte), cont),
 
     0xFF: function () {
       console.log('HALT');
@@ -311,4 +187,43 @@ var
 exports.runInstruction = function (ins) {
   var opcode = ins >>> 24;
   (insTable[opcode])(ins);
+};
+
+// for testing
+exports.resetStatus = function (ins) {
+  regs = [];
+  for (var i = 0; i < 16; i++) {
+    regs.push(new Buffer(4));
+  }
+  fregs = [];
+  for (var i = 0; i < 16; i++) {
+    fregs.push(new Buffer(8));
+  }
+  mem = {};
+  PC = 0;
+};
+
+exports.setRegisters = function (obj) {
+  for (var i = 0; i < 16; i++) {
+    if (obj[i]) {
+      regs[i] = obj[i];
+    }
+  }
+};
+
+exports.setFRegisters = function (obj) {
+  for (var i = 0; i < 16; i++) {
+    if (obj[i]) {
+      fregs[i] = obj[i];
+    }
+  }
+};
+
+exports.fetchStatus = function () {
+  return {
+    'regs': regs.slice(),
+    'fregs': fregs.slice(),
+    'mem': JSON.parse(JSON.stringify(mem)),
+    'pc': PC
+  }
 };
---
+++ simulator/binary.js
@@ -0,0 +1,171 @@
+var m = {};
+
+// (Number -> Number -> Number) -> Function
+var int32 = function (op) {
+  // Buffer -> Buffer -> Buffer
+  return function (a, b) {
+    var num1 = a.readInt32LE(0, 4);
+    var num2 = b.readInt32LE(0, 4);
+    var num3 = op(num1, num2);
+    var c = new Buffer(4);
+    c.writeInt32LE(num3, 0, 4);
+    return c;
+  };
+};
+
+// (Number -> Number -> Number) -> Function
+var uint32 = function (op) {
+  // Buffer -> Buffer -> Buffer
+  return function (a, b) {
+    var num1 = a.readUInt32LE(0, 4);
+    var num2 = b.readUInt32LE(0, 4);
+    var num3 = op(num1, num2);
+    var c = new Buffer(4);
+    c.writeUInt32LE(num3, 0, 4);
+    return c;
+  };
+};
+
+// Number -> Buffer
+m.ext32 = function (imm) {
+  var a = new Buffer(2);
+  a.writeUInt16LE(imm, 0, 2);
+  var b = new Buffer(4);
+  b.writeInt32LE(a.readInt16LE(0, 2), 0, 4);
+  return b;
+};
+
+m.uext32 = function (imm) {
+  var buf = new Buffer(4);
+  buf.writeUInt32LE(imm, 0, 4);
+  return buf;
+};
+
+m.oext32 = function (imm) {
+  var a = m.ext32(imm);
+  var b = a.readInt32LE(0, 4);
+  a.writeInt32LE(b << 2, 0, 4);
+  return a;
+};
+
+m.add32 = int32(function (a, b) {
+  return (a + b) << 0;
+});
+
+m.sub32 = int32(function (a, b) {
+  return (a - b) << 0;
+});
+
+m.mul32 = int32(function (a, b) {
+  return (a * b) << 0;
+});
+
+m.div32 = int32(function (a, b) {
+  return (Math.floor(a / b)) << 0;
+});
+
+m.mod32 = int32(function (a, b) {
+  return (a % b) << 0;
+});
+
+m.and32 = int32(function (a, b) {
+  return a & b;
+});
+
+m.or32 = int32(function (a, b) {
+  return a | b;
+});
+
+m.not32 = int32(function (a) {
+  return ~a;
+});
+
+m.xor32 = int32(function (a, b) {
+  return a ^ b;
+});
+
+m.shl32 = int32(function (a, b) {
+  return a << b;
+});
+
+m.sar32 = int32(function (a, b) {
+  return a >> b;
+});
+
+m.slr32 = int32(function (a, b) {
+  return a >>> b;
+});
+
+m.eq32 = int32(function (a, b) {
+  return (a == b) ? 1 : 0;
+});
+
+m.ne32 = int32(function (a, b) {
+  return (a != b) ? 1 : 0;
+});
+
+m.lt32 = int32(function (a, b) {
+  return (a < b) ? 1 : 0;
+});
+
+m.ltu32 = uint32(function (a, b) {
+  return (a < b) ? 1 : 0;
+});
+
+m.le32 = int32(function (a, b) {
+  return (a <= b) ? 1 : 0;
+});
+
+m.leu32 = uint32(function (a, b) {
+  return (a <= b) ? 1 : 0;
+});
+
+m.gt32 = int32(function (a, b) {
+  return (a > b) ? 1 : 0;
+});
+
+m.gtu32 = uint32(function (a, b) {
+  return (a > b) ? 1 : 0;
+});
+
+m.ge32 = int32(function (a, b) {
+  return (a >= b) ? 1 : 0;
+});
+
+m.geu32 = uint32(function (a, b) {
+  return (a >= b) ? 1 : 0;
+});
+
+// Number -> Number -> Function
+var load = function (size, bytes) {
+  // Number -> Object -> Buffer
+  return function (addr, mem) {
+    var buf = new Buffer(size);
+    for (var i = 0; i < bytes; i++) {
+      buf[i] = mem[addr + i][0];
+    }
+    return buf;
+  };
+};
+
+m.loadWord = load(4, 4);
+m.loadHalf = load(4, 2);
+m.loadByte = load(4, 1);
+m.loadFloat = load(8, 8);
+
+// Number -> Function
+var store = function (bytes) {
+  // Number -> Buffer -> Object
+  return function (addr, data, mem) {
+    for (var i = 0; i < bytes; i++) {
+      mem[addr + i][0] = data[i];
+    }
+  };
+};
+
+m.storeWord = store(4);
+m.storeHalf = store(2);
+m.storeByte = store(1);
+m.storeFloat = store(8);
+
+exports = m;
