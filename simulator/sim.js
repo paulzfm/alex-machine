23bc75305a5a386e4aa79c9130332d72a20df65e
Paul Zhu <zhufengminpaul@163.com>
2016-05-02 12:34:10 +0800

    add load BSS

--- simulator/sim.js
+++ simulator/sim.js
@@ -11,11 +11,21 @@ fs.readFile('/Users/paul/Workspace/alex-cpu-test/a.out', function (err, data) {
   var text = (sections.filter(function (obj) {
     return obj.name == '.text';
   }))[0];
+  var bss = (sections.filter(function (obj) {
+    return obj.name == '.bss';
+  }))[0];
+
+  cpu.resetStatus();
+
+  // load BSS
+  cpu.initMemory(bss.data);
+
+  // run instructions
   var instructions = text.data;
   var total = instructions.length / 4;
   for (var i = 0; i < total; i++) {
     var ins = instructions.readUInt32LE(i * 4);
-    console.log(ins.toString(16));
+    //console.log(ins.toString(16));
     cpu.runInstruction(ins);
   }
 });
